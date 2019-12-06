6e94f401bdccd5ec43c96a718c3d17ee73bfd49b
ann1403 <32761721+ann1403@users.noreply.github.com>
2019-12-06 02:01:06 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -8,29 +8,30 @@ const iPhone = puppeteer.devices['iPhone 6'];
 
 server.listen(3000);
 
-
-puppeteer.launch().then(async browser => {
-    const page = await browser.newPage();
-    await page.emulate(iPhone);
-    await page.goto('https://dou.ua/lenta/');
-    await page.screenshot({ path: 'example.png' });
-
-    await page.click('body > div.g-page > div.l-content.m-content > div > div.col70.m-cola > div > div > div.col50.m-cola > div.b-lenta > article:nth-child(1) > h2 > a');
-    await page.waitFor(1000);
-
-    const result = await page.evaluate(() => {
-        let title = document.querySelector('h1').innerText;
-        let text = document.querySelector('p').innerText;
-        return {
-            title,
-            text
+try {
+    puppeteer.launch().then(async browser => {
+        for (let i = 1; i < 3; i++) {
+            const page = await browser.newPage();
+            await page.goto('https://dou.ua/lenta/');
+            await page.click('body > div.g-page > div.l-content.m-content > div > div.col70.m-cola > div > div > div.col50.m-cola > div.b-lenta > article:nth-child(' + i + ') > h2 > a');
+            await page.waitFor(1000);
+            const result = await page.evaluate(() => {
+                let title = document.querySelector('h1').innerText;
+                let text = document.querySelector('p').innerText;
+                let article = { title, text };
+                return article;
+            });
+            io.sockets.on('art', function(socket) {
+                socket.json.send(result);
+            })
+            await browser.close();
+            console.log(result)
         }
-    });
-    // other actions...
-    await browser.close();
-    console.log(result)
-});
 
+    });
+} catch {
+    if (err) throw err;
+}
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html', function(err, data) {
 
